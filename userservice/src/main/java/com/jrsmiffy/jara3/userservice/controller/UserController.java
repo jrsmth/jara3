5954171a0f2091234dc3d0b0baec480d7e3bf5c5
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-02-09 17:27:02 +0000

    updated user service

--- userservice/src/main/java/com/jrsmiffy/jara3/userservice/controller/UserController.java
+++ userservice/src/main/java/com/jrsmiffy/jara3/userservice/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.jrsmiffy.jara3.userservice.controller;
 
 import com.jrsmiffy.jara3.userservice.model.User;
-import com.jrsmiffy.jara3.userservice.repository.UserRepository;
-import com.netflix.discovery.EurekaClient;
+import com.jrsmiffy.jara3.userservice.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,49 +18,68 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    /**
-     * Adding Client Service Discovery Capabilities
-     */
     @Autowired
-    @Lazy
-    private EurekaClient eurekaClient;
+    private UserService userService;
 
-    @Value("${spring.application.name}")
-    private String appName;
+    /** Register A New User */
+    @RequestMapping(path = "/user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<Map<HttpStatus, Object>> registerUser(@RequestBody User potentialUser) {
 
-    @Autowired
-    private UserRepository userRepository;
+        // Validate that this user is valid - if so, register them
+        User registeredUser = userService.registerNewUser(potentialUser);
 
-    /**
-     * Login - Verify User Credentials & Accept/Reject Login Request
-     */
-    @RequestMapping(path = "/login/{username}/{password}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<Map<HttpStatus, Object>> login(@PathVariable("username") String username, @PathVariable("password") String password) {
+        // Response
         Map<HttpStatus, Object> returnObject = new HashMap<>();
-        returnObject.put(HttpStatus.OK, "Connection confirmed");
+        returnObject.put(HttpStatus.OK, registeredUser);
+
         return ResponseEntity.ok(returnObject);
     }
 
-    /**
-     * Create User
-     */
-    @RequestMapping(path = "/user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<Map<HttpStatus, Object>> createUser(@RequestBody User newUser) {
 
-        // Are these credentials valid? -> they have already been validated in the frontend
 
-        Map<HttpStatus, Object> returnObject = new HashMap<>();
-        try{
-            // Does this user already exist?
-            userRepository.findByUsername(newUser.getUsername()); // wtf is this logic...
-            userRepository.save(newUser);
-        } catch (NullPointerException e){
-            log.error(e.toString());
-        } finally {
-            returnObject.put(HttpStatus.OK, newUser);
-            return ResponseEntity.ok(returnObject);
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    /**
+//     * Login - Verify User Credentials & Accept/Reject Login Request
+//     */
+//        @RequestMapping(path = "/login/{username}/{password}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//        public @ResponseBody ResponseEntity<Map<HttpStatus, Object>> login(@PathVariable("username") String username, @PathVariable("password") String password) {
+//            Map<HttpStatus, Object> returnObject = new HashMap<>();
+//            returnObject.put(HttpStatus.OK, "Connection confirmed");
+//            return ResponseEntity.ok(returnObject);
+//        }
+//
+//        /**
+//         * Create User
+//         */
+//        @RequestMapping(path = "/user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+//        public @ResponseBody
+//        ResponseEntity<Map<HttpStatus, Object>> createUser(@RequestBody User newUser) {
+//
+//            // Are these credentials valid? -> they have already been validated in the frontend
+//
+//            Map<HttpStatus, Object> returnObject = new HashMap<>();
+//            try{
+//                // Does this user already exist?
+//                userRepository.findByUsername(newUser.getUsername()); // wtf is this logic...
+//                userRepository.save(newUser);
+//            } catch (NullPointerException e){
+//                log.error(e.toString());
+//            } finally {
+//                returnObject.put(HttpStatus.OK, newUser);
+//                return ResponseEntity.ok(returnObject);
+//            }
+//    }
 
 
 
--- userservice/src/main/java/com/jrsmiffy/jara3/userservice/service/UserService.java
+++ userservice/src/main/java/com/jrsmiffy/jara3/userservice/service/UserService.java
@@ -1,8 +1,21 @@
 package com.jrsmiffy.jara3.userservice.service;
 
+import com.jrsmiffy.jara3.userservice.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
+
+    public User registerNewUser(User potentialUser){
+
+        log.info("hit me baby one more time");
+
+        return new User();
+    }
 
 }
