26d9c28681707a466b38b36bdd198917cd179323
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-03-07 18:12:13 +0000

    finish user controller tdd

--- userservice/src/main/java/com/jrsmiffy/jara3/userservice/controller/UserController.java
+++ userservice/src/main/java/com/jrsmiffy/jara3/userservice/controller/UserController.java
@@ -1,55 +1,75 @@
 package com.jrsmiffy.jara3.userservice.controller;
 
+import com.jrsmiffy.jara3.userservice.model.User;
+import com.jrsmiffy.jara3.userservice.model.UserResponse;
 import com.jrsmiffy.jara3.userservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
 
     private final UserService userService;
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-//    /** Register New User */
-//    @RequestMapping(path = "/register", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public @ResponseBody ResponseEntity<Map<HttpStatus, Object>> register(@RequestBody final User potentialUser) {
-//
-//        // Check that this potential user is valid - if so, register them; else, return exception
-//        final Map<HttpStatus, Object> returnObject = userService.register(potentialUser);
-//
-//        return ResponseEntity.ok(returnObject);
-//    }
-//
-//
-//    /** Authenticate User */
-//    @RequestMapping(path = "/authenticate/{username}/{password}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public @ResponseBody ResponseEntity<Map<HttpStatus, Object>> authenticate(@PathVariable("username") final String username, @PathVariable("password") final String password) {
-//
-//        // Check that these user credentials match with a valid user - if so, return success; else, return err
-//        final Map<HttpStatus, Object> returnObject = userService.authenticate(username, password);
-//
-//        return ResponseEntity.ok(returnObject);
-//
-//    }
-//
-//    /** Get All Users - Dev Purposes */
-//    @RequestMapping(path = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public @ResponseBody
-//    ResponseEntity<Object> getAllUsers() {
-//        return ResponseEntity.ok(userService.getAllUsers());
-//    }
+    /** Register User */
+    @PostMapping(path = "/register")
+    public ResponseEntity<Object> register(@RequestBody final User potentialUser) {
 
+        // Check that this potential user is valid - if so, register them; else, return err
+        ResponseEntity<Object> response;
+        Map<HttpStatus, UserResponse> responseMap = new HashMap<>();
+
+        final UserResponse userResponse = userService.register(potentialUser);
+
+        if(userResponse.getUser().isPresent())
+            responseMap.put(HttpStatus.OK, userResponse);
+        else if (userResponse.getUser().isEmpty())
+            responseMap.put(HttpStatus.BAD_REQUEST, userResponse);
+
+        response = ResponseEntity.ok(responseMap);
+        log.info(response.toString());
+
+        return response;
+    }
+
+
+    /** Authenticate User */
+    @GetMapping(path = "/authenticate/{username}/{password}")
+    public ResponseEntity<Object> authenticate(@PathVariable("username") final String username, @PathVariable("password") final String password) {
+
+        // Check that these user credentials match with a valid user - if so, return success; else, return err
+        ResponseEntity<Object> response;
+        Map<HttpStatus, UserResponse> responseMap = new HashMap<>();
+
+        final UserResponse userResponse = userService.authenticate(username, password);
+
+        if(userResponse.getUser().isPresent())
+            responseMap.put(HttpStatus.OK, userResponse);
+        else if (userResponse.getUser().isEmpty())
+            responseMap.put(HttpStatus.BAD_REQUEST, userResponse);
+
+        response = ResponseEntity.ok(responseMap);
+        log.info(response.toString());
+
+        return response;
+    }
 
     /** Get All Users - Dev Purposes */
     @RequestMapping(path = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<Object> getAllUsers() {
+    public ResponseEntity<Object> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
--- userservice/src/test/java/com/jrsmiffy/jara3/userservice/controller/UserControllerTest.java
+++ userservice/src/test/java/com/jrsmiffy/jara3/userservice/controller/UserControllerTest.java
@@ -1,4 +1,113 @@
 package com.jrsmiffy.jara3.userservice.controller;
 
+import com.jrsmiffy.jara3.userservice.model.User;
+import com.jrsmiffy.jara3.userservice.model.UserResponse;
+import com.jrsmiffy.jara3.userservice.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    private UserController underTest;
+
+    private ResponseEntity<Object> expectedResponse;
+
+    private Map<HttpStatus, UserResponse> responseMap = new HashMap<>();
+
+    @BeforeEach
+    void setUp() {
+        underTest = new UserController(userService);
+    }
+
+    @Test
+    @DisplayName("Should Register User")
+    void shouldRegisterUser() {
+
+        // Given: a valid potential user (that passes the checks)
+        User validPotentialUser = new User(UUID.randomUUID(), "username", "password", true);
+        UserResponse mockedResponse = new UserResponse(Optional.of(validPotentialUser), "response");
+        responseMap.put(HttpStatus.OK, mockedResponse);
+        expectedResponse = ResponseEntity.ok(responseMap);
+
+        // When: mock the service call with validPotentialUser & get the result from register()
+        when(userService.register(validPotentialUser)).thenReturn(mockedResponse);
+        ResponseEntity<Object> actualResult = underTest.register(validPotentialUser);
+
+        // Then: check that this result is equal to the expected
+        assertThat(actualResult).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("Should Not Register User - Checks Failed")
+    void shouldNotRegisterUserBecauseChecksFailed() {
+
+        // Given: an invalid potential user, where the checks fail
+        User invalidPotentialUser = new User(UUID.randomUUID(), "username", "password", true);
+        UserResponse mockedResponse = new UserResponse(Optional.empty(), "response");
+        responseMap.put(HttpStatus.BAD_REQUEST, mockedResponse);
+        expectedResponse = ResponseEntity.ok(responseMap);
+
+        // When: mock the service call with invalid user & get the result from register()
+        when(userService.register(invalidPotentialUser)).thenReturn(mockedResponse);
+        ResponseEntity<Object> actualResult = underTest.register(invalidPotentialUser);
+
+        // Then: check that this result is equal to the expected
+        assertThat(actualResult).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("Should Authenticate User")
+    void shouldAuthenticateUser() {
+
+        // Given: a valid potential user (that passes the checks)
+        User validPotentialUser = new User(UUID.randomUUID(), "username", "password", true);
+        UserResponse mockedResponse = new UserResponse(Optional.of(validPotentialUser), "response");
+        responseMap.put(HttpStatus.OK, mockedResponse);
+        expectedResponse = ResponseEntity.ok(responseMap);
+
+        // When: mock the service call with validPotentialUser & get the result from authenticate()
+        when(userService.authenticate("username", "password")).thenReturn(mockedResponse);
+        ResponseEntity<Object> actualResult = underTest.authenticate("username", "password");
+
+        // Then: check that this result is equal to the expected
+        assertThat(actualResult).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("Should Not Authenticate User - Checks Failed")
+    void shouldNotAuthenticateUserBecauseChecksFailed() {
+
+        // Given: an invalid potential user, where the checks fail
+        User invalidPotentialUser = new User(UUID.randomUUID(), "username", "password", true);
+        UserResponse mockedResponse = new UserResponse(Optional.empty(), "response");
+        responseMap.put(HttpStatus.BAD_REQUEST, mockedResponse);
+        expectedResponse = ResponseEntity.ok(responseMap);
+
+        // When: mock the service call with invalid user & get the result from authenticate()
+        when(userService.authenticate("username", "password")).thenReturn(mockedResponse);
+        ResponseEntity<Object> actualResult = underTest.authenticate("username", "password");
+
+        // Then: check that this result is equal to the expected
+        assertThat(actualResult).isEqualTo(expectedResponse);
+    }
+
+
 }
