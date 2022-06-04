c22acaf6f61ca9b4edeba2661cb9a301098818f9
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-06-04 11:15:44 +0100

    user svc refactor

--- user-service/src/main/java/com/jrsmiffy/jara3/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/jrsmiffy/jara3/userservice/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.jrsmiffy.jara3.userservice.repository;
 
 import com.jrsmiffy.jara3.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
